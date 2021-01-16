e2d709dd80b010b617ac3bc0b13512d79caa3e3a
Rob <robrichardsdpt@gmail.com>
2021-01-16 12:29:36 -0500

    added ball animation

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-window.alert('hello')
+document.querySelector("#cube").addEventListener('click',function ()
+{
+ if (document.querySelector("#cube").style.animation.includes("pause")) {
+  (document.querySelector("#cube").style.animation = "cubeRotation 2s linear infinite")
+ } else {
+  (document.querySelector("#cube").style.animation = "pause")
+ }
+}  )
+
