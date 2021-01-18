3370255ff25cbcede0ced990005f6041706ddd3a
Rob <robrichardsdpt@gmail.com>
2021-01-18 09:12:08 -0500

    added countdown timer

--- index.js
+++ index.js
@@ -12,3 +12,27 @@ document.querySelector(".image").addEventListener('click',function ()
 {
   (document.querySelector(".image").style.animation = "desaturate 4s linear forwards")
 }  )
+
+
+function startTimer(duration, display) {
+  var timer = duration, minutes, seconds;
+  setInterval(function () {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.textContent = minutes + ":" + seconds;
+
+      if (--timer < 0) {
+          timer = duration;
+      }
+  }, 1000);
+}
+
+window.onload = function () {
+  var oneMinute = 60 * 1,
+      display = document.querySelector('#time');
+  startTimer(oneMinute, display);
+};
