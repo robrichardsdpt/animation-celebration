1bef74de74a9ab84228bcb3645fa9f9c33068fee
Rob <robrichardsdpt@gmail.com>
2021-02-05 10:42:23 -0500

    updated js

--- form-interaction.js
+++ form-interaction.js
@@ -9,22 +9,22 @@ form.addEventListener('submit', (event) => {
     colorArray.push(Math.floor(Math.random() * numberInput))
   }
   const color = colorArray.join('')
-  console.log(color)
   let i = numberInput * 2000
   let j = 1
   document.body.style.background = `#${color}`
   const timer = setInterval(function () {
     const newDiv = document.createElement("div")
     newDiv.classList.add("ball")
-    newDiv.id = `ball${j}`
+    newDiv.classList.add(`ball${j}`)
     newDiv.style.top = `${j*175}px`
     newDiv.style.left = `${j*175}px`
     newDiv.style.zIndex = `${j}`
-    const currentDiv = document.querySelector(`#ball${j-1}`)
+    const currentDiv = document.querySelector(`.ball${j-1}`)
     j++
     document.body.insertBefore(newDiv, currentDiv)
-    const k = Math.floor(Math.random() * j)
-    newDiv.style.animation = `ballmovement 3s linear alternate 10 forwards`
+    const k = Math.floor(Math.random() * 2)
+    if (k< 1) newDiv.style.animation = `ballmovement 3s linear alternate 10 forwards`
+    if (k >= 1) newDiv.style.animation = `ballmovementjump 3s linear alternate 10 forwards`
   }, 2000)
   setTimeout(function( ) { clearInterval( timer ) }, i)
 })
