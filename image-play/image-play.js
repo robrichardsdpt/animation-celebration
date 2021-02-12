401fe4a4142023261833f15d963a46afec5d3e71
Rob <robrichardsdpt@gmail.com>
2021-02-12 09:09:41 -0500

    added image-play

--- image-play/image-play.js
+++ image-play/image-play.js
@@ -2,7 +2,8 @@ document.body.onload = function() {
   let i = 0
   const timer = setInterval(function () {
     const newDiv = document.createElement("div")
-    newDiv.classList.add("square")
+    newDiv.classList.add("square1")
+    newDiv.classList.add("square1")
     newDiv.id = `square${i}`
     let leftMath = (225 * i) - 225
     newDiv.style.left = `${leftMath}px`
@@ -17,5 +18,18 @@ document.body.onload = function() {
 }
 
 const changeClassToSink = () => {
-  document.querySelector('img').classList.add("sink")
+  let i = 0
+  const timer = setInterval(function () {
+    const changeDivClass = document.getElementById(`square${i}`)
+    changeDivClass.classList.remove("square1")
+    changeDivClass.classList.add("square2")
+    i++
+  }, 2000)
+  setTimeout(function( ) { clearInterval( timer ) }, 10000)
+  setTimeout(function() { changeClassToGrayscale() }, 2000)
+}
+
+const changeClassToGrayscale = () => {
+  const changeDivClass = document.querySelector(`img`)
+  changeDivClass.classList.add("image")
 }
