55509b4f79cbed48d541b9bc66380f9daf37f5ca
Rob <robrichardsdpt@gmail.com>
2021-02-22 09:18:45 -0500

    javascript world conference animation 1 finished.

--- js-world/index.js
+++ js-world/index.js
@@ -1,25 +1,34 @@
-const arrayOfBlocks = [0,1,2,3,4,5,6,7,8,9]
+let canvas;
+let context;
 
-const mergeSort = (arr) => {
-  if(arr.length <= 1) return arr
-  let middle = Math.floor(arr.length/2)
-  let leftArr = arr.slice(0, middle)
-  let rightArr = arr.slice(middle, arr.length)
-  return merge(mergeSort(leftArr), mergeSort(rightArr))
+window.onload = init;
+
+function init(){
+  canvas = document.getElementById('canvas');
+  context = canvas.getContext('2d');
+  // Start the first frame request
+  window.requestAnimationFrame(gameLoop);
 }
 
-const merge = (left, right) => {
-  const mergedArr = []
-  let leftIndex = 0
-  let rightIndex = 0
-  while(leftIndex < left.length && rightIndex < right.length) {
-    if(left[leftIndex] < right[rightIndex]){
-      mergedArr.push(left[leftIndex])
-      leftIndex++
-    } else {
-      mergedArr.push(right[rightIndex])
-      rightIndex++
-    }
-  }
-  return mergedArr.concat(left.slice(leftIndex), right.slice(rightIndex))
+
+
+function draw() {
+  context.fillStyle = 'gold';
+  context.fillRect(rectX, rectY, -150, -100);
+}
+
+let rectX = 0;
+let rectY = 0;
+
+function gameLoop(timeStamp) {
+    // Update game objects in the loop
+    update();
+    draw();
+
+    window.requestAnimationFrame(gameLoop);
+}
+
+function update() {
+    rectX += 2;
+    rectY += 1;
 }
