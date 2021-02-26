9cce39f8e2820e5f7c0019640db142a524336bab
Rob <robrichardsdpt@gmail.com>
2021-02-26 16:40:16 -0500

    code more dry

--- voice-activated-color/index.js
+++ voice-activated-color/index.js
@@ -1,28 +1,28 @@
 function runSpeechRecognition() {
   // get output div reference
-  var output = document.getElementById("output");
+  var output = document.getElementById("output")
   // get action element reference
-  var action = document.getElementById("action");
+  var action = document.getElementById("action")
       // new speech recognition object
-      var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-      var recognition = new SpeechRecognition();
+      var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+      var recognition = new SpeechRecognition()
   
       // This runs when the speech recognition service starts
       recognition.onstart = function() {
-          action.innerHTML = "<small>Pick a color, any color...</small>";
-      };
+          action.innerHTML = "<small>Pick a color, any color...</small>"
+      }
       
       recognition.onspeechend = function() {
-          action.innerHTML = "<small>Completed!</small>";
-          recognition.stop();
+          action.innerHTML = "<small>Completed!</small>"
+          recognition.stop()
       }
     
       // This runs when the speech recognition service returns result
       recognition.onresult = function(event) {
-          var transcript = event.results[0][0].transcript;
-          var confidence = event.results[0][0].confidence;
-          output.innerHTML = "<b>Text:</b> " + transcript + "<br/> <b>Confidence:</b> " + confidence*100+"%";
-          output.classList.remove("hide");
+          var transcript = event.results[0][0].transcript
+          var confidence = event.results[0][0].confidence
+          output.innerHTML = "<b>Text:</b> " + transcript + "<br/> <b>Confidence:</b> " + confidence*100+"%"
+          output.classList.remove("hide")
           const colors = ['red', 'green', 'blue', 'purple', 'orange', 'gray', 'pink', 'cyan', 'maroon', 'gold', 'teal', 'silver', 'chartreuse', 'black', 'yellow', 'brown']
           const lowerCaseTranscript = transcript.split(' ').map(word => { return word.toLowerCase() })
           const speechContainer = document.querySelector('.speech-container')
@@ -36,5 +36,5 @@ function runSpeechRecognition() {
       document.querySelector('.speech-container').classList.remove('dance')
       document.querySelector('.speech-container').classList.remove('spin')
        // start recognition
-       recognition.start();
+       recognition.start()
 }
