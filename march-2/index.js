0b522b495c2da596d7e41be2bb2341fa1bc6de58
Rob <robrichardsdpt@gmail.com>
2021-03-03 17:32:22 -0500

    balls change color with toggle.

--- march-2/index.js
+++ march-2/index.js
@@ -7,10 +7,16 @@ const handleClick = () => {
   toggleLight = !toggleLight
   if(toggleLight) {
     document.querySelector('.container').style.background = 'lightsteelblue'
+    document.querySelector('#circle1').style.background = 'darkred'
+    document.querySelector('#circle2').style.background = 'navy'
+    document.querySelector('#circle3').style.background = 'black'
     lighten.innerHTML = 'Get Dark!'
   }  
   if(!toggleLight) {
     document.querySelector('.container').style.background = 'black'
+    document.querySelector('#circle1').style.background = 'blanchedalmond'
+    document.querySelector('#circle2').style.background = 'burlywood'
+    document.querySelector('#circle3').style.background = 'darkgoldenrod'
     lighten.innerHTML = 'Lighten Up!'
   }
 }
